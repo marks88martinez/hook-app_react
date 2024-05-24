af97de30cdc1814531e1b91108a9bd1aed561cab
marcos <marks88martinez@gmail.com>
2024-05-24 06:04:04 -0400

    save localstorage

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,23 +1,31 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { todoReducer } from './todoReducer'
 import { TodoList } from "./TodoList"
 import { TodoAdd } from "./TodoAdd"
  
 const initialState = [
-    {
-        id: new Date().getTime(),
-        description: ' Recolectar la piedra del alma',
-        done: false,
-    },
-    {
-        id: new Date().getTime() + 100,
-        description: ' Recolectar la piedra del tiempo',
-        done: false,
-    },
+    // {
+    //     id: new Date().getTime(),
+    //     description: ' Recolectar la piedra del alma',
+    //     done: false,
+    // },
+
 ]
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || []
+}
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState)
+
+  
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos) || [])
+
+    }, [todos])
+    
 
     const handleNewTodo = ( todo ) => {
         const action = {
